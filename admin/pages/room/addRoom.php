da89b47057d4ab343a84752f5f906a7c3b0c6f77
Monparty <sukontaprapun@gmail.com>
2023-12-30 02:11:21 +0700

    edit dashboard,addroom

--- admin/pages/room/addRoom.php
+++ admin/pages/room/addRoom.php
@@ -10,19 +10,12 @@ $price = $_POST["price"];
 $roomabout = $_POST["roomabout"];
 $bed = $_POST["bed"];
 $amountpeople = $_POST["amountpeople"];
+$facility = implode(",", $_POST["facility"]);
+$highlight = implode(",", $_POST["highlight"]);
+$status = implode(",", $_POST["status"]);
 
-$facility = $_POST["facility"];
-$facility_string = implode(",", $facility);
-
-$highlight = $_POST["highlight"];
-$highlight_string = implode(",", $highlight);
-
-$status = $_POST["status"];
-$status_string = implode(",", $status);
-
-
-$sql = "INSERT INTO rooms (name, roomdesc, price, roomabout,  bed, amountpeople, facility, highlight, status) 
-                    VALUES (:name, :roomdesc, :price, :roomabout, :bed, :amountpeople, :facility_string, :highlight_string, :status_string)";
+$sql = "INSERT INTO rooms (name, roomdesc, price, roomabout,  bed, amountpeople, facility, highlight, status, createAt)
+VALUES (:name, :roomdesc, :price, :roomabout, :bed, :amountpeople, :facility, :highlight, :status, CURRENT_TIMESTAMP)";
 
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(":name", $name);
@@ -30,16 +23,24 @@ $stmt->bindParam(":roomdesc", $roomdesc);
 $stmt->bindParam(":price", $price);
 $stmt->bindParam(":roomabout", $roomabout);
 $stmt->bindParam(":bed", $bed);
-/*
 $stmt->bindParam(":amountpeople", $amountpeople);
-$stmt->bindParam(":facility_string", $facility);
-$stmt->bindParam(":highlight_string", $highlight);
-$stmt->bindParam(":status_string", $status);
-*/
+$stmt->bindParam(":facility", $facility);
+$stmt->bindParam(":highlight", $highlight);
+$stmt->bindParam(":status", $status);
 $stmt->execute();
 
 if ($stmt->rowCount() > 0) {
-  echo "บันทึกข้อมูลสำเร็จ";
+  echo '<script>
+      setTimeout(function() {
+      swal({
+          title: "บันทึกข้อมูลสำเร็จ",  
+          type: "success"
+      }, function() {
+          window.location = "roomList.php";
+      });
+      }, 1000);
+    </script>';
+  
 } else {
   echo "เกิดข้อผิดพลาดในการบันทึกข้อมูล";
 }
