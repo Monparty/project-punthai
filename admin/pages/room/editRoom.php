d2f2f0f31ec21bc41ae40fd71243cf4ef8998185
monsuniti <sukontaprapun@gmail.com>
2024-01-07 16:43:12 +0700

    Commit editRoom success

--- admin/pages/room/editRoom.php
+++ admin/pages/room/editRoom.php
@@ -2,11 +2,38 @@
 include("../../../config/config.php");
 session_start();
 
-$id = mysqli_real_escape_string($c,$_GET['id']);
-$sql = "SELECT * FROM rooms WHERE id='$id' ";
-$result = mysqli_query($c, $sql) or die(mysqli_error($c));
+// ใช้สำหรับแสดงข้อมูลตาม ID ที่เลือกมา
+$id = $_REQUEST['id'];
+$sql = "SELECT * FROM rooms WHERE id=$id";
+$result = mysqli_query($c, $sql);
 $row = mysqli_fetch_array($result);
 extract($row);
+
+
+// ใช้สำหรับ Update ข้อมูลจะทำงานเมื่อกดปุ่ม update
+if (isset($_REQUEST['update'])) {
+    $id = $_POST['id'];
+    $sql = "UPDATE rooms SET name=:name WHERE id=$id";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":name", $name);
+    $name = $_POST['name'];
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+    echo '<script>
+        setTimeout(function() {
+        swal({
+            title: "แก้ไขข้อมูลสำเร็จ",  
+            type: "success"
+        }, function() {
+            window.location = "roomList.php";
+        });
+        }, 1000);
+        </script>';
+    } else {
+    echo "เกิดข้อผิดพลาดในการแก้ไขข้อมูล";
+    }
+}
 
 include ("editRoom.html");
 ?>
