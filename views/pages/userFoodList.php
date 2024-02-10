7922cf5101ce24ace9776e4509b5cbf6f63b7e56
monsuniti <sukontaprapun@gmail.com>
2024-02-10 20:01:50 +0700

    commit module food

--- views/pages/userFoodList.php
+++ views/pages/userFoodList.php
@@ -2,7 +2,7 @@
 include ("../../config/config.php");
 session_start();
 
-$sql = "SELECT * FROM rooms WHERE status = 'Active'";
+$sql = "SELECT * FROM foods WHERE food_status = 'Active'";
 $query = mysqli_query( $c, $sql );
 
 // ใช้แสดงรูปภาพ
@@ -13,14 +13,14 @@ $fetch = mysqli_fetch_assoc($query);
 foreach ($stmt as $i=>$fetch) {
 
 // แปลงข้อมูลรูปภาพจากฐานข้อมูลเป็นฐาน64
-$image_base64[$i] = $fetch["image"];
+$image_base64[$i] = $fetch["food_image"];
 
 // แปลงข้อมูลรูปภาพจากฐาน64เป็นข้อมูลรูปภาพ
-$image = base64_decode($image_base64[$i]);
+$food_image = base64_decode($image_base64[$i]);
 
 // แสดงผลรูปภาพ
-$showimg[$i] = '<img src="data:$image/png;base64,' . $image_base64[$i] . '" style="width: 200px; height: 180px; object-fit: cover; border-radius: 4px; transition: .3s;"/>';
+$showimg[$i] = '<img src="data:$food_image/png;base64,' . $image_base64[$i] . '" style="width: 220px; height: 180px; object-fit: cover; border-radius: 4px; transition: .3s;"/>';
 }
 
-include ("roomList.html");
+include ("userFoodList.html");
 ?>
